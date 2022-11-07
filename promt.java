a84dd9ae6fbf0eb329991c8103f9f6f5266422ff
Jonas Dåderman <jonasdad@kth.se>
2022-11-07 12:03:06 +0100

    Added correct csv file

--- promt.java
+++ promt.java
@@ -2,5 +2,8 @@ import java.util.*;
 public class promt {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("**********Welcome to Periodic Table Practice**********");
+        System.out.println("          What would you like to do?         ");
+        System.out.println("1. Show perodic table\n2. Practice beteckningar\n3. Practice Atomic Masses\n4. Quit");
     }
 }
