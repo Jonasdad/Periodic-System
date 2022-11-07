7e28b4dc81ff796cfb466bb28ed10d01634f8e87
Jonas Dåderman <jonasdad@kth.se>
2022-11-07 12:00:00 +0100

    Added input and file reader in periodicsystem

--- periodicSystem.java
+++ periodicSystem.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class periodicSystem {
-    
+    public periodicSystem(String file) {
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- promt.java
+++ promt.java
@@ -1,5 +1,6 @@
+import java.util.*;
 public class promt {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
     }
 }
