6a3d3e6e52a4dea3a28c4d62bc23b3b5b6c7013e
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-11-07 12:10:33 +0100

    element done

--- element.java
+++ element.java
@@ -1,7 +1,18 @@
 public class element{
+    String symbol;
     String elementName;
-    int elementNumber;
-    
+    double atomicMass;
+    boolean metalOrNah;
+
+    //atomic number will be array index+1,symbol,name,atomic mass,metal or nonmetal?
+    public element(String symbol, String elementName, double atomicMass, boolean metalOrNah){
+        this.symbol = symbol;
+        this.elementName = elementName;
+        this.atomicMass = atomicMass;
+        this.metalOrNah = metalOrNah; 
+    }
+
+
     
     
 }
