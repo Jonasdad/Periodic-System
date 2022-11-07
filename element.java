c18d165416816b0d60cbb0b5ec0504a81e189c6b
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-11-07 12:29:13 +0100

    ok

--- element.java
+++ element.java
@@ -1,11 +1,11 @@
-public class element{
+public class Element{
     String symbol;
     String elementName;
     double atomicMass;
     boolean metalOrNah;
 
     //atomic number will be array index+1,symbol,name,atomic mass,metal or nonmetal?
-    public element(String symbol, String elementName, double atomicMass, boolean metalOrNah){
+    public Element(String symbol, String elementName, double atomicMass, boolean metalOrNah){
         this.symbol = symbol;
         this.elementName = elementName;
         this.atomicMass = atomicMass;
--- periodicSystem.java
+++ periodicSystem.java
@@ -2,13 +2,27 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 public class periodicSystem {
     public periodicSystem(String file) {
+        Element elementArray[] = new Element[118];
+        int i = 0;
         
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
+            boolean checkMetal;
 
             while ((line = br.readLine()) != null) {
                 String[] row = line.split(",");
+
+                if(row[4] == "yes"){
+                    checkMetal = true;
+                } else {
+                    checkMetal = false;
+                }
+
+                elementArray[i] = new Element(row[1], row[2], Double.parseDouble(row[3]),checkMetal);
+                i++; 
             }
+
+
         } catch (Exception e) {
             System.out.println(e);
         }
